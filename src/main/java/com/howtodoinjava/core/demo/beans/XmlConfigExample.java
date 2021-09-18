a74c44a208aee37b077ae3db908bbf9222b8d9c8
Anuj <apathak1912@gmail.com>
2021-09-18 21:54:40 +0530

    Second commit

--- src/main/java/com/howtodoinjava/core/demo/beans/XmlConfigExample.java
+++ src/main/java/com/howtodoinjava/core/demo/beans/XmlConfigExample.java
@@ -11,7 +11,7 @@ public class XmlConfigExample
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/howtodoinjava/core/demo/beans/beans.xml");
         
         Employee employee = ctx.getBean(Employee.class);
-         
+        employee.setName("Anuj");
         Department department = ctx.getBean(Department.class);
          
         Operations operations = ctx.getBean(Operations.class);
